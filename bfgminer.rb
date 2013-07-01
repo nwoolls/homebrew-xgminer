2a9f9f4b8d801eb62106df24ce5d9d4145ab5bea
Nate Woolls <nwoolls@gmail.com>
2013-07-01 15:12:50 -0400

    Added the ability to instead from the repo head by doing brew install bfgminer --HEAD

--- bfgminer.rb
+++ bfgminer.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Bfgminer < Formula
   homepage 'https://github.com/luke-jr/bfgminer'
+  head 'https://github.com/luke-jr/bfgminer.git', :branch => 'bfgminer'
   url 'http://luke.dashjr.org/programs/bitcoin/files/bfgminer/3.1.1/bfgminer-3.1.1.zip'
   sha1 '43054174042775268d5bc8a084eecb60420d7c81'
 
@@ -14,6 +15,7 @@ class Bfgminer < Formula
   depends_on 'jansson'
 
   def install
+    system "./autogen.sh"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "PKG_CONFIG_PATH=/usr/local/opt/curl/lib/pkgconfig:/usr/local/opt/jansson/lib/pkgconfig",
