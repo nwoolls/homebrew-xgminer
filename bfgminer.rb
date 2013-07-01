7bd680c8e45ee7cf4c9432d9d3ed3ef736d61060
Nate Woolls <nwoolls@gmail.com>
2013-07-01 12:02:54 -0400

    Updated formula for bfgminer 3.1.1, requires uthash

--- bfgminer.rb
+++ bfgminer.rb
@@ -2,13 +2,14 @@ require 'formula'
 
 class Bfgminer < Formula
   homepage 'https://github.com/luke-jr/bfgminer'
-  url 'http://luke.dashjr.org/programs/bitcoin/files/bfgminer/3.0.2/bfgminer-3.0.2.zip'
-  sha1 '43e01b571407d4a92dfdf9ca8b215fe3a1cc3fde'
+  url 'http://luke.dashjr.org/programs/bitcoin/files/bfgminer/3.1.1/bfgminer-3.1.1.zip'
+  sha1 '43054174042775268d5bc8a084eecb60420d7c81'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
   depends_on 'libtool' => :build
   depends_on 'pkg-config' => :build
+  depends_on 'uthash' => :build
   depends_on 'curl'
   depends_on 'jansson'
 
