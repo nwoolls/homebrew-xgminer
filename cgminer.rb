f8ff0f5d093776972b2fde3bf093a8ad6aa14af8
Nate Woolls <nwoolls@gmail.com>
2013-09-13 08:55:26 -0400

    Updated cgminer to 3.4.3

--- cgminer.rb
+++ cgminer.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Cgminer < Formula
   homepage 'https://github.com/ckolivas/cgminer'
   head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
-  url 'https://github.com/ckolivas/cgminer/archive/v3.3.4.tar.gz'
-  sha1 '2c6ba42e3a37a51de232e128203bdfd3dc1e2342'
+  url 'https://github.com/ckolivas/cgminer/archive/v3.4.3.tar.gz'
+  sha1 'e936497d334ea0bcd4cd1cb75db35fe43e25b097'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
@@ -16,7 +16,9 @@ class Cgminer < Formula
   depends_on 'libusb'
 
   def install
+    inreplace "autogen.sh", "libtoolize", "glibtoolize"
     inreplace "autogen.sh", "readlink", "greadlink"
+    system "autoreconf -fvi"
     system "./autogen.sh", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "PKG_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/curl/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/jansson/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/libusb/lib/pkgconfig",
