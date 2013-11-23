ca0c934e3215c6def5d75ffeb2bebfd00f151e10
Nate Woolls <nwoolls@gmail.com>
2013-11-23 13:02:49 -0500

    Updated cgminer to 3.8.3

--- cgminer.rb
+++ cgminer.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Cgminer < Formula
   homepage 'https://github.com/ckolivas/cgminer'
   head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
-  url 'https://github.com/ckolivas/cgminer/archive/v3.8.2.tar.gz'
-  sha1 'bb67a403b1234679cc71192507b086eb2bb60147'
+  url 'https://github.com/ckolivas/cgminer/archive/v3.8.3.tar.gz'
+  sha1 '3f310e75ca92315f7feb6d9540b5ac0813c8d91f'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
