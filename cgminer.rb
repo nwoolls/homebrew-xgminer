de6034339c5183491c6f42b18c2e3af5ed2825d7
Nate Woolls <nwoolls@gmail.com>
2014-11-04 02:21:40 -0500

    Update CGMiner to 4.7.1

--- cgminer.rb
+++ cgminer.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Cgminer < Formula
   homepage 'https://github.com/ckolivas/cgminer'
   head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
-  url 'https://github.com/ckolivas/cgminer/archive/v4.6.1.tar.gz'
-  sha1 'c72a571368ff064e7ecdb2d33539fbcdd5d73214'
+  url 'https://github.com/ckolivas/cgminer/archive/v4.7.1.tar.gz'
+  sha1 '80cba0dd735d9b9593cfcc4763daa79887ac104b'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
@@ -30,11 +30,11 @@ class Cgminer < Formula
                           "--enable-bitfury",
                           "--enable-hashfast",
                           "--enable-klondike",
-                          "--enable-bab",
                           "--enable-drillbit",
-                          "--enable-hashfast",
                           "--enable-avalon2",
-                          "--enable-minion"
+                          "--enable-blockerupter",
+                          "--enable-cointerra",
+                          "--enable-hashratio"
     system "make", "install"
   end
 
