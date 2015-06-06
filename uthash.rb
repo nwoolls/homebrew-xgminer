62b3d9a81ccc6b83848d86b5199917b46ad0df2e
Nate Woolls <nwoolls@gmail.com>
2015-06-06 11:15:18 -0400

    Fix sha1 for uthash - author modified an archived release

--- uthash.rb
+++ uthash.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Uthash < Formula
   homepage 'http://troydhanson.github.io/uthash/'
   url 'https://github.com/troydhanson/uthash/archive/58d5990617.zip'
-  sha1 '015622dfee4367b5de07a7207f7beb3768e0bd99'
+  sha1 'dda1f1116d41e190990cff1cb11c654daee1d256'
   version '58d5990617'
 
   def install
