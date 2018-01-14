41ed15922591a89a11ce64113a6918b791ed38de
Nate Woolls <nwoolls@gmail.com>
2018-01-13 22:26:04 -0500

    Update uthash to 2.0.2

--- uthash.rb
+++ uthash.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Uthash < Formula
   homepage 'http://troydhanson.github.io/uthash/'
-  url 'https://github.com/troydhanson/uthash/archive/58d5990617.zip'
-  sha256 '1ddd5c11027133ea3275c53640f3acbcd833d6b1d23acbe6e3f71a802ef8a795'
-  version '58d5990617'
+  url 'https://github.com/troydhanson/uthash/archive/v2.0.2.zip'
+  sha256 '1c56f7e75ee42e162fb3f72cede7738118c3e6fb88514d40db6afc952dcd7cae'
+  version '2.0.2'
 
   def install
     include.install Dir['src/*']
