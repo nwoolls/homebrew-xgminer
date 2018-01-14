c7906bc381a432b7e783472124ac7a626fcf6346
Nate Woolls <nwoolls@gmail.com>
2018-01-13 22:26:57 -0500

    Update BFGMiner to 5.5

--- bfgminer.rb
+++ bfgminer.rb
@@ -3,8 +3,9 @@ require 'formula'
 class Bfgminer < Formula
   homepage 'https://github.com/luke-jr/bfgminer'
   head 'https://github.com/luke-jr/bfgminer.git', :branch => 'bfgminer'
-  url 'http://luke.dashjr.org/programs/bitcoin/files/bfgminer/5.1.0/bfgminer-5.1.0.zip'
-  sha256 '7b56f7f59dbb019972641dc252372c5235e0b6433bdcfd6f6966d3087346bbe2'
+  url 'http://luke.dashjr.org/programs/bitcoin/files/bfgminer/5.5.0/bfgminer-5.5.0.zip'
+  sha256 '67e716b04e0bebb311d6e805e1e1fbabca2187a7d9fb05e38e1f8447d6cb72ac'
+  version '5.5.0'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
@@ -17,9 +18,13 @@ class Bfgminer < Formula
   depends_on 'libevent'
   depends_on 'libusb'
   depends_on 'hidapi'
+  depends_on 'gnu-sed'
 
   def install
+    inreplace "gen-version.sh", "sed", "gsed"
+    system "chmod +x *.sh"
     system "NOSUBMODULES=1 ./autogen.sh"
+    system "chmod +x configure"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "PKG_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/curl/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/jansson/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/libmicrohttpd/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/libusb/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/hidapi/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/libevent/lib/pkgconfig",
